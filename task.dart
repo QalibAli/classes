c05a8d01c95b604c4ff3ca289fcc5a45c23967da
Qalib Alizade <qalibalizade@Qalibs-MacBook-Air.local>
2024-03-11 22:34:27 +0400

    task

---
+++ task.dart
@@ -0,0 +1,37 @@
+/*
+    Task 1
+
+  "Texts" adında class yaradın. Elə bir şərt qoyun ki , class'dan obyek yaratmaq mümkün olmasın.
+  Class'ın içində bir neçə  text yazıb main.dart'in içində çağırın.
+
+*/
+
+
+/*
+      Task 2
+
+  'Operations' adında Class açın içində 5 lik vurma cədvəlinin çıxaracaq loop yazın.
+  Main' də methodu işlədin.
+
+  5 lik vurma cədvəli
+
+  5x1=5
+  5x2=10
+  5x3=15
+  5x4=20
+  5x4=25
+  5x5=30... 10 a qədər.
+
+
+*/
+
+
+
+/* 
+     Task 3
+
+Xüsusiyyətləri [id, ad, qiymət] olan bir class yaratmaq üçün dart proqramı yazın. 
+Onun konstruktorunu yaradın və ondan 3 obyekt yaradın. 
+Onları siyahıya əlavə edin və bütün detalları çap edin.  (for vasitesile)
+
+*/
